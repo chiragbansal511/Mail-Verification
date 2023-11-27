e3d433c34ebc97be6dccf9930f04cc1cd5f583cd
chirag bansal <chirageng.stu22@gmail.com>
2023-11-28 00:04:25 +0530

    verification script

--- mail.js
+++ mail.js
@@ -1,20 +1,29 @@
 const nodemailer = require('nodemailer');
+const express = require("express");
+const app = express();
+const path = require('path');
+const bodyparser = require("body-parser");
+let code = 0;
 
+
+app.use(bodyparser.urlencoded({extended : true}));
+app.use(bodyparser.json());
 
 const transporter = nodemailer.createTransport({
-  service: 'senders address', 
+  service: 'chiragbansal112004@gmail.com', 
   auth: {
-    user: 'senders address', 
-    pass: 'App password', 
+    user: 'chiragbansal112004@gmail.com', 
+    pass: "osei mgmo bdzc pizk", 
   },
 });
-
-
-const mailOptions = {
-    from: 'senders address', // Sender's email address
-    to: 'receiver address', // Recipient's email address
-    subject: 'subject', // Email subject
-    text: 'message', // Email body (plain text)
+ 
+function send(receiveradd , verificationcode)
+{
+  const mailOptions = {
+    from: 'chiragbansal112004@gmail.com', 
+    to: receiveradd, 
+    subject: 'subject', 
+    text: `${verificationcode}`,
   };
   
 
@@ -24,5 +33,33 @@ const mailOptions = {
         } else {
           console.log('Email sent:', info.response);
         }
-      });
-      
+      });   
+}
+
+function verificationmessage()
+{
+const verificationcode = Math.floor(Math.random()*8999 + 1000);
+send("chiragbansal511@gmail.com" , verificationcode);
+console.log(verificationcode);
+return verificationcode;
+}
+
+app.get('/' , (req , res)=>{
+
+  res.sendFile(path.join(__dirname, '/index.html'));
+  code = verificationmessage();
+});
+
+
+app.post('/' , (req , res)=>{
+  const entercode = req.body.code;
+  console.log(entercode);
+     if(code == entercode)
+     res.send("sucess");
+
+     else res.send("failed");
+})
+
+app.listen(80 , ()=>{
+  console.log("server started");
+});
